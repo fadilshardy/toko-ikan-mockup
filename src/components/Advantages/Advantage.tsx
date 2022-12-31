f9177f4246ca22f2e4b10181ca0c3d035f01d70d
fadilshardy <fadilshrdy@gmail.com>
2023-01-01 03:11:21 +0700

    feat: add advantages section

--- src/components/Advantages/Advantage.tsx
+++ src/components/Advantages/Advantage.tsx
@@ -8,12 +8,12 @@ interface IAdvantageProps {
 }
 
 const Advantage: React.FunctionComponent<IAdvantageProps> = ({ icon, title, description }) => {
-    return <div>
-        <div className="relative">
+    return <div className="border border-gray-200 p-20 flex flex-col items-center rounded-2xl">
+        <div className="relative w-24 h-24">
             <Image src={icon} alt={title} fill={true}></Image>
         </div>
-        <h4>{title}</h4>
-        <p>{description}</p>
+        <h4 className="text-2xl py-4 text-center font-semibold">{title}</h4>
+        <p className="text-lg text-neutral-500 leading tracking-wide text-center">{description}</p>
     </div>;
 };
 
--- src/components/Advantages/Advantages.tsx
+++ src/components/Advantages/Advantages.tsx
@@ -6,21 +6,21 @@ interface IAdvantagesProps {
 
 const advantages = [
     { icon: "images/box.svg", title: "Kirim Ke Berbagai Negara", description: "A Lorem Ipsum is simply dummy text of the printing and typesetting industry" },
-    { icon: "images/user-click.svg", title: "Kirim Ke Berbagai Negara", description: "A Lorem Ipsum is simply dummy text of the printing and typesetting industry" },
-    { icon: "images/guard.svg", title: "Kirim Ke Berbagai Negara", description: "A Lorem Ipsum is simply dummy text of the printing and typesetting industry" }
+    { icon: "images/user-click.svg", title: "Mudah Di Gunakan", description: "A Lorem Ipsum is simply dummy text of the printing and typesetting industry" },
+    { icon: "images/guard.svg", title: "Pengiriman Aman", description: "A Lorem Ipsum is simply dummy text of the printing and typesetting industry" }
 ]
 
 const Advantages: React.FunctionComponent<IAdvantagesProps> = (props) => {
-    return <section>
-        <div>
-            <h2>Keunggulan kami</h2>
-            <p>A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy text ever since the 1500s.</p>
+    return <section className="py-28 container mx-auto">
+        <div className="flex flex-col items-center pb-28">
+            <h2 className="font-bold text-4xl lg:text-5xl pb-8">Keunggulan kami</h2>
+            <p className="font-medium text-neutral-400 max-w-xl mx-auto text-center">A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy text ever since the 1500s.</p>
         </div>
-        <ul>
+        <div className="flex gap-8 flex-col lg:flex-row px-2">
             {advantages.map(advantage => (
                 <Advantage key={advantage.icon} icon={advantage.icon} title={advantage.title} description={advantage.description} />
             ))}
-        </ul>
+        </div>
     </section>;
 };
 
